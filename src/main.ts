e666c14c66a5a00ee4aba196496431bc79a65109
星如雨 <liyun200104@163.com>
2024-02-27 09:39:34 +0800

    🎈 perf(router): formatter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 
 import ElementPlus from 'element-plus'
-import routes from 'virtual:generated-pages'
 import App from './App.vue'
+import router from './router'
 
 import '@unocss/reset/tailwind.css'
 import 'element-plus/dist/index.css'
@@ -13,10 +12,7 @@ import './styles/main.css'
 import 'uno.css'
 
 const app = createApp(App)
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-})
+
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
